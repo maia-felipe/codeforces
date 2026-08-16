89797b33705f74f82b83152c13268b3398b4835f
maiafelipe0911 <fe.maia@klauvi.com>
2026-08-16 18:55:07 -0300

    Add solution for problem 2029 - Stone Game IX

---
+++ leetcode/2029. Stone Game IX.cpp
@@ -0,0 +1,27 @@
+/*
+Alice and Bob continue their games with stones. There is a row of n stones, and each stone has an associated value. 
+You are given an integer array stones, where stones[i] is the value of the ith stone.
+
+Alice and Bob take turns, with Alice starting first. On each turn, the player may remove any stone from stones. 
+The player who removes a stone loses if the sum of the values of all removed stones is divisible by 3. 
+Bob will win automatically if there are no remaining stones (even if it is Alice's turn).
+
+Assuming both players play optimally, return true if Alice wins and false if Bob wins.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int freq[3]={0};
+        for(int x: stones){
+            freq[x%3]++;
+        }
+        const bool f0=(freq[0]&1)==1;
+        if (f0==0) return freq[1]>0 && freq[2]>0;
+        const int diff=abs(freq[1]-freq[2]);
+        return diff>=3;
+    }
+};
